1e85e37b651a145649a932a577b19e3c7280c1a6
Abel Vargas <abelvargas@pm.me>
2020-03-14 16:44:49 -0700

    Katana Laucher Script with Redshift with Katana Resources updated

---
+++ Katana/launchRedshift4Katana.bat
@@ -0,0 +1,18 @@
+rem Redshift for Katana script
+
+set "KATANA_VERSION=3.5v2"
+
+set "KATANA_HOME=C:\Program Files\Katana%KATANA_VERSION%"
+set "REDSHIFT_HOME=C:\ProgramData\Redshift\bin"
+set "REDSHIFT4KATANA_HOME=C:\ProgramData\Redshift\Plugins\Katana\3.2v1"
+
+set DEFAULT_RENDERER=Redshift
+set "KATANA_TAGLINE=With Redshift 2.6"
+
+set REDSHIFT_CACHE_BUDGET=
+set REDSHIFT_CACHE_FOLDER=
+set REDSHIFT_SELECTED_CUDA_DEVICES=
+
+set "path=%REDSHIFT_HOME%;%path%"
+set "KATANA_RESOURCES=%REDSHIFT4KATANA_HOME%;C:\Users\abelv\Documents\KatanaHomePipeline\KatanaResources"
+"%KATANA_HOME%\bin\katanaBin.exe"
