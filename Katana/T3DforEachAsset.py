ec71131135d3154cefe576e9d60e08a6f300965e
Abel Vargas <abelvargas@pm.me>
2020-06-07 00:33:53 -0700

    Working T3D for each asset

--- Katana/T3DforEachAsset.py
+++ Katana/T3DforEachAsset.py
@@ -3,34 +3,37 @@ from Katana import NodegraphAPI
 from Katana import ScenegraphManager
 
 selectedGroups = ScenegraphManager.getActiveScenegraph().getSelectedLocations()
-print selectedGroups[0]
+baseNode = NodegraphAPI.GetNode(NodegraphAPI.GetAllSelectedNodes()[0].getName())
+nodePosition = NodegraphAPI.GetNodePosition(baseNode)
 
-t3d_grp = NodegraphAPI.CreateNode('Group', NodegraphAPI.GetRootNode())
-t3d_grp.setName("T3Ds")
-t3d_grp.addOutputPort('out')
-t3d_grp_port = abc_grp.getReturnPort('out')
+x = 0
+y = 50
+xpos = nodePosition[0] + x
+ypos = nodePosition[1] - y
 
-mergeNode = NodegraphAPI.CreateNode("Merge", t3d_grp)
-mergeNode.setName('T3D_Merge')
-mergeInputIndex = 0
-mergePosition = NodegraphAPI.GetNodePosition(mergeNode)
-xStartPos = mergePosition[0]
-yStartPos = mergePosition[1]
-XIncrementPosition = 0
-print XIncrementPosition
-xpos = xStartPos + XIncrementPosition
-ypos = yStartPos + 50
+dot = NodegraphAPI.CreateNode("Dot", NodegraphAPI.GetRootNode())
+NodegraphAPI.SetNodePosition(dot, [xpos,ypos])
+y += 50
+dot.getInputPort('input').connect(baseNode.getOutputPort('default'))
+dot.setName("T3D_dot")
+dot.addOutputPort("out")
+NodegraphAPI.SetAllSelectedNodes('')
+NodegraphAPI.SetNodeSelected(dot, True)
+
 
 for i in selectedGroups:
-    merge_port = mergeNode.addInputPort('i'+str(mergeInputIndex))
-    mergePosition = NodegraphAPI.GetNodePosition(mergeNode)
 
-    t3d = NodegraphAPI.CreateNode("Transform3D", t3d_grp)
+    y = 50
+    previousNode = NodegraphAPI.GetNode(NodegraphAPI.GetAllSelectedNodes()[0].getName())
+    previousNodePosition = NodegraphAPI.GetNodePosition(previousNode)
+    xpos = previousNodePosition[0]
+    ypos = previousNodePosition[1] - y
+    previousNodePort = previousNode.getOutputPort('out')
+    t3d = NodegraphAPI.CreateNode("Transform3D", NodegraphAPI.GetRootNode())
     t3d.getParameter("path").setValue(i,0)
     t3d.getParameter("makeInteractive").setValue("Yes",0)
-
     NodegraphAPI.SetNodePosition(t3d, [xpos,ypos])
-
-    t3d.getOutputPort('out').connect(merge_port)
-    mergeInputIndex += 1
-    XIncrementPosition += 50
+    t3d.getInputPort('in').connect(previousNodePort)
+    NodegraphAPI.SetAllSelectedNodes('')
+    NodegraphAPI.SetNodeSelected(t3d, True)
+    y += 50
