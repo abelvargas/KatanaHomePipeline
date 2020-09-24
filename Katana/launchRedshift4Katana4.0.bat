c2bb4d37b9d54d4736768d2c5605d60df8abe981
Abel Vargas <abelvargas@pm.me>
2020-09-23 20:51:29 -0700

    Adding latest katana launchers

---
+++ Katana/launchRedshift4Katana4.0.bat
@@ -0,0 +1,17 @@
+rem Katana3.7 with 3Delight and USD
+
+set "KATANA_VERSION=4.0v1.010011b"
+set "DEFAULT_RENDERER=dl"
+
+set "KATANA_HOME=C:\Program Files\Katana%KATANA_VERSION%"
+set "DELIGHT=C:\Program Files\3Delight"
+set "DLFK_INSTALL_PATH=%DELIGHT%\3DelightForKatana"
+set "DL_DISPLAYS_PATH=%DELIGHT%\displays"
+set "DL_SHADERS_PATH=%DELIGHT%\shaders"
+
+set "KATANA_TAGLINE=3Delight/USD"
+
+set "PATH=%PATH%;%KATANA_HOME%\bin;%KATANA_HOME%\plugins\Resources\Usd\lib;C:\Program Files\3Delight\bin"
+set "KATANA_RESOURCES=C:\Users\abelv\Documents\KatanaHomePipeline\KatanaResources;%KATANA_HOME%\plugins\Resources\Usd\plugin;%DLFK_INSTALL_PATH%"
+
+"%KATANA_HOME%\bin\katanaBin.exe"
