ce2e2d003dcb7621bfb4cd6b47332bb066a2d644
Abel Vargas <abelvargas@pm.me>
2020-09-28 14:42:26 -0700

    Updating OCIO env variable

--- Katana/launchKatana4.0-3dl-NoOCIO.bat
+++ Katana/launchKatana4.0-3dl-NoOCIO.bat
@@ -9,8 +9,8 @@ set "DLFK_INSTALL_PATH=%DELIGHT%\3DelightForKatana"
 set "DL_DISPLAYS_PATH=%DELIGHT%\displays"
 set "DL_SHADERS_PATH=%DELIGHT%\shaders"
 
-set "KATANA_TAGLINE=3Delight + USD + OCIO"
-set "OCIO=C:\Program Files\OCIO\aces_1.0.3\config.ocio"
+set "KATANA_TAGLINE=3Delight + USD - No OCIO"
+
 set "PATH=%PATH%;%KATANA_HOME%\bin;%KATANA_HOME%\plugins\Resources\Usd\lib;C:\Program Files\3Delight\bin"
 set "KATANA_RESOURCES=C:\Users\abelv\Documents\KatanaHomePipeline\KatanaResources;%KATANA_HOME%\plugins\Resources\Usd\plugin;%DLFK_INSTALL_PATH%"
 
