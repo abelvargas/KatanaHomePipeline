a966d05ef3075a99122440f1c754866bda81e7ad
Abel Vargas <abelvargas@pm.me>
2021-06-16 23:02:11 -0700

    Adding comments to Batch Render

--- Katana/PythonTab_BatchRender.py
+++ Katana/PythonTab_BatchRender.py
@@ -1,4 +1,6 @@
-cd Program Files\Katana4.0v2\bin
+### This script is a way to batch render from Katana's interactive mode ###
+# Copy and paste this script into your python tab. Make sure to update RenderNode
+# and frame range as noted below!
 
 import NodegraphAPI
 from Katana import KatanaFile
@@ -6,14 +8,14 @@ from Katana import RenderManager
 def messageHandler( sequenceID, message ):
   print message
 
-RenderNode = NodegraphAPI.GetNode('BG') # Getting Render node
+RenderNode = NodegraphAPI.GetNode('BG') # UPDATE with the name of your render node.
 renderSettings = RenderManager.RenderingSettings()
 renderSettings.frame=1001
 renderSettings.mode=RenderManager.RenderModes.DISK_RENDER
 renderSettings.asynchRenderMessageCB=messageHandler
 renderSettings.asynch=False
 
-for frame in range(1001, 1050):
+for frame in range(1001, 1050): #UPDATE WITH YOUR FRAME RANGE!!!
     print '-' * 80
     print '\nRendering Node "%s" frame %s...' % (RenderNode.getName(), frame)
     renderSettings.frame = frame
